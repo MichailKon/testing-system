63b160801089a55c11ff5a07d939af5e7be47c91
Philip Gribov <grphil@grphil.ru>
2025-01-07 01:48:19 +1100

    Add request creation

--- lib/common/testing_system.go
+++ lib/common/testing_system.go
@@ -2,6 +2,7 @@ package common
 
 import (
 	"github.com/gin-gonic/gin"
+	"github.com/go-resty/resty/v2"
 	"strconv"
 	"testing_system/lib/config"
 	"testing_system/lib/logger"
@@ -35,4 +36,22 @@ func (ts *TestingSystem) Run() {
 	if err != nil {
 		logger.Panic(err.Error())
 	}
+}
+
+// NewMasterRequest creates new request to master with authentication and address set up.
+// master address will be taken from config (it is either to same process, or to the other microservice)
+func (ts *TestingSystem) NewMasterRequest() *resty.Request {
+	// TODO: Add request creation
+}
+
+// NewStorageRequest creates new request to storage with authentication and address set up.
+// master address will be taken from config (it is either to same process, or to the other microservice)
+func (ts *TestingSystem) NewStorageRequest() *resty.Request {
+	// TODO: Add request creation
+}
+
+// NewInvokerRequest creates new request to invoker with authentication set up.
+// params may be changed depending on how invokers are stored, probably address will be set up by caller
+func (ts *TestingSystem) NewInvokerRequest(invokerID string) *resty.Request {
+	// TODO: Add request creation
 }
