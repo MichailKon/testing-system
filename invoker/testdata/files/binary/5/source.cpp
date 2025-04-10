ab4684b3e5f92cf9c7cbb1936316ed7d9323ad26
Philip Gribov <grphil@grphil.ru>
2025-04-10 16:09:52 +0300

    Fix testdata

--- invoker/testdata/files/binary/5/source.cpp
+++ invoker/testdata/files/binary/5/source.cpp
@@ -2,7 +2,7 @@
 
 int main() {
   int res = 0;
-  for (long long i = 0; i < 1000000000000; i++) {
+  for (long long i = 1; i < 1000000000000; i++) {
     res += i;
     if (res % i == 0) {
       res--;
