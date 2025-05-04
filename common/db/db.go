24168fc6fafbcbeb2151d03ff06bfb7eaf47c48b
Philip Gribov <grphil@mail.ru>
2025-05-04 10:00:05 +0300

    Small fix

--- common/db/db.go
+++ common/db/db.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	"fmt"
 	"gorm.io/driver/postgres"
 	"gorm.io/driver/sqlite"
 	"gorm.io/gorm"
@@ -32,7 +31,6 @@ func NewDB(config config.DBConfig) (*gorm.DB, error) {
 }
 
 func newPostgresDB(config config.DBConfig) (*gorm.DB, error) {
-	fmt.Println(config.Dsn)
 	db, err := gorm.Open(postgres.Open(config.Dsn), &gorm.Config{})
 	if err != nil {
 		return nil, logger.Error("Can't open database with dsn=\"%v\" because of %v:", config.Dsn, err)
